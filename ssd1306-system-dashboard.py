f4e561c62234a058f5babf20d6f3cce1465948b1
Mehmethan AYTEKELİ <76856067+mehmethanaytekeli@users.noreply.github.com>
2025-04-30 16:31:27 +0300

    Create ssd1306-system-dashboard.py

---
+++ ssd1306-system-dashboard.py
@@ -0,0 +1,68 @@
+import time
+import psutil
+import netifaces
+from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
+from luma.oled.device import ssd1306
+from luma.core.interface.serial import i2c
+
+# Ekranı başlatıyoruz
+serial = i2c(port=1, address=0x3C)
+disp = ssd1306(serial)
+
+# Font ayarı
+font = ImageFont.load_default()
+
+# Görüntü oluşturma
+image = Image.new('1', (disp.width, disp.height))
+draw = ImageDraw.Draw(image)
+
+# Yazma işlemi için konum
+x = 0
+y = 4
+
+# Sonraki okuma için eski ağ verisi
+prev_recv = psutil.net_io_counters().bytes_recv
+prev_sent = psutil.net_io_counters().bytes_sent
+
+def format_speed(speed):
+    if speed > 1024:
+        return f"{speed / 1024:.1f}MB/s"
+    else:
+        return f"{speed:.1f}KB/s"
+
+while True:
+    # Bilgileri toplama
+    cpu_temp = psutil.sensors_temperatures()['cpu_thermal'][0].current
+    ip_address = netifaces.ifaddresses('eth0')[netifaces.AF_INET][0]['addr']
+    ram_usage = psutil.virtual_memory().percent
+    current_time = datetime.now().strftime('%H:%M:%S')
+    cpu_usage = psutil.cpu_percent(interval=0.1)
+
+  # Ağ hızı bilgisi
+    curr_recv = psutil.net_io_counters().bytes_recv
+    curr_sent = psutil.net_io_counters().bytes_sent
+    download_speed = (curr_recv - prev_recv) / 1024
+    upload_speed = (curr_sent - prev_sent) / 1024
+    prev_recv, prev_sent = curr_recv, curr_sent
+
+    # Ekranı temizle
+    draw.rectangle((0, 0, disp.width, disp.height), outline=0, fill=0)
+
+    # Bilgileri çiz
+    draw.text((x, y), f"   Time: {current_time}", font=font, fill=255)
+    draw.text((x, y + 10), f"CPU: {round(cpu_temp)}°C", font=font, fill=255)  # Değişen satır
+    draw.text((x, y + 20), f"Usage: {cpu_usage}%", font=font, fill=255)
+    draw.text((x, y + 30), f"IP: {ip_address}", font=font, fill=255)
+    draw.text((x, y + 40), f"RAM: {ram_usage}%", font=font, fill=255)
+
+    # Ok çizimleri
+    arrow_y = y + 52
+    draw.polygon([(0, arrow_y+5), (4, arrow_y+5), (2, arrow_y+2)], fill=255)  # ▼
+    draw.polygon([(64, arrow_y+2), (68, arrow_y+2), (66, arrow_y+5)], fill=255)  # ▲
+    draw.text((6, arrow_y-2), format_speed(upload_speed), font=font, fill=255)
+    draw.text((70, arrow_y-2), format_speed(download_speed), font=font, fill=255)
+
+    # Ekranı güncelle
+    disp.display(image)
+    time.sleep(1)
